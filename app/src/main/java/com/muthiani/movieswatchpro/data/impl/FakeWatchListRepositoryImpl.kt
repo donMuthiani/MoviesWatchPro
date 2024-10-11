caebc1ec7965314641f8b86ce3058eb5eecea171
donmuthiani <donmuthiani@gmail.com>
2024-10-11 14:36:35 +0300

    discover screen

--- app/src/main/java/com/muthiani/movieswatchpro/data/impl/FakeWatchListRepositoryImpl.kt
+++ app/src/main/java/com/muthiani/movieswatchpro/data/impl/FakeWatchListRepositoryImpl.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class FakeWatchListRepositoryImpl @Inject constructor() : FakeWatchListRepository {
     private val watchlist = listOf(
         Movie(
-            id =1,
+            id = 1,
             title = "Dune",
             description = "A noble family becomes embroiled in a war for control of the galaxy's most valuable asset while its heir becomes troubled by visions of a dark future.",
             imageUrl = "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
@@ -71,7 +71,7 @@ class FakeWatchListRepositoryImpl @Inject constructor() : FakeWatchListRepositor
             progress = "5"
         ),
         Movie(
-            id= 7,
+            id = 7,
             title = "Spider-Man: Into the Spider-Verse",
             description = "Teen Miles Morales becomes Spider-Man of his reality, crossing his path with five counterparts from other dimensions to stop a threat for all realities.",
             imageUrl = "https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_FMjpg_UX1000_.jpg",
---
+++ app/src/main/java/com/muthiani/movieswatchpro/ui/myshows/MyShowsScreen.kt
@@ -0,0 +1,56 @@
+package com.muthiani.movieswatchpro.ui.myshows
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.muthiani.movieswatchpro.ui.components.customHomeTopBar
+import com.muthiani.movieswatchpro.ui.home.WatchListViewModel
+
+@Composable
+fun MyShowsScreen(innerPAdding: PaddingValues, navController: NavController) {
+
+    val watchListViewModel: WatchListViewModel = hiltViewModel()
+    val myShows by watchListViewModel.uiState.collectAsState()
+
+    Scaffold(topBar = { customHomeTopBar(showActions = false) }, content = { innerPadding ->
+        LazyColumn(modifier = Modifier.padding(innerPadding)) {
+            items(myShows.watchList.filter { it.progress.toInt() > 50 }) { movie ->
+                Row(modifier = Modifier.padding(8.dp)) {
+                    AsyncImage(model = movie.imageUrl, contentDescription = "",
+                        modifier = Modifier
+                            .height(120.dp)
+                            .width(80.dp)
+                            .clip(RoundedCornerShape(4.dp)))
+
+                    Spacer(modifier = Modifier.size(16.dp))
+
+                    Column {
+                        Text(text = movie.title, style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding(top = 4.dp))
+                    }
+                }
+            }
+        }
+    }, bottomBar = {})
+}
